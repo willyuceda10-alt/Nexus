178a6309f65e98386a4a065f5f09b3d964031c11
willyuceda10-alt <willy.uceda.10@gmail.com>
2026-08-27 09:46:02 -0500

    infra: include board option dictionaries in tenant RLS

--- prisma/sql/tenant_rls.sql
+++ prisma/sql/tenant_rls.sql
@@ -62,7 +62,9 @@ BEGIN
     'work_board_groups_v1',
     'work_board_columns_v1',
     'work_views_v1',
-    'work_board_item_placements_v1'
+    'work_board_item_placements_v1',
+    'work_board_option_sets_v1',
+    'work_board_options_v1'
   ]
   LOOP
     -- This script is also used as a defensive re-application step. During
