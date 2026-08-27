37ba3af8ee6d63d258b3d9321cab4faae411e6c9
willyuceda10-alt <willy.uceda.10@gmail.com>
2026-08-26 23:48:35 -0500

    security: add notification preferences to tenant RLS

--- prisma/sql/tenant_rls.sql
+++ prisma/sql/tenant_rls.sql
@@ -56,7 +56,8 @@ BEGIN
     'automation_step_runs_v1',
     'automation_approval_requests_v1',
     'inbox_items_v1',
-    'notification_deliveries_v1'
+    'notification_deliveries_v1',
+    'notification_preferences_v1'
   ]
   LOOP
     -- This script is also used as a defensive re-application step. During
